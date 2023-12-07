b8ed68531e02b7ab22cc514ea65edd107f8c829d
Ankit Maurya <96623227+am-1145@users.noreply.github.com>
2023-12-07 19:45:18 +0530

    Add files via upload

---
+++ src/mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://@cluster0.venzvjb.mongodb.net/LoginFormPractice?retryWrites=true&w=majority")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
